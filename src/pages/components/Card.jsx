6504c7426fd5417abd3e1aeff930d7de1a2820c0
Prafulpradhan11 <Prafulpradhan1113@gamil.com>
2025-04-10 13:45:36 +0545

    Home Page

---
+++ src/pages/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+     <>
+         <div class="max-w-sm rounded overflow-hidden shadow-lg my-10">
+  <img class="w-full" src="https://media.istockphoto.com/id/1475868329/vector/pile-text-books-library.jpg?s=612x612&w=0&k=20&c=ltDiyt-E7KXw912huPJ-Q-L-0S_gtC_BmFX69UJa95I=" alt="Book Image"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Book Name</div>
+    <p class="text-gray-700 text-base">
+      Book description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+</div>
+     </>
+  )
+}
+
+export default Card
