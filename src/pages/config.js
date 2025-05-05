3b4494f0f73994e99ab844503a81076e143f82f2
Prafulpradhan11 <Prafulpradhan1113@gamil.com>
2025-05-05 15:54:05 +0545

    lets see

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,2 +1,2 @@
-export const backendUrl = 'https://mern2-0-basicnode-7zq5.onrender.com/book'
-// http://localhost:3000/book
+export const backendUrl = 'https://mern2-0-basicnode-7zq5.onrender.com'
+// export const backendUrl = 'http://localhost:3000'
